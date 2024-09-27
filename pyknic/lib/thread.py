5687649e15e88955edac9002f42d17d4cd4fe2ba
Ildar Gafurov <dev@binblob.com>
2024-09-27 19:23:05 +0300

    new module -- lib/tasks/thread.py and its tests (copied from wasp-general)

---
+++ pyknic/lib/thread.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+# pyknic/lib/thread.py
+#
+# Copyright (C) 2017-2024 the pyknic authors and contributors
+# <see AUTHORS file>
+#
+# This file is part of pyknic.
+#
+# pyknic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyknic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pyknic.  If not, see <http://www.gnu.org/licenses/>.
+
+import functools
+import threading
+import types
+import typing
+
+import decorator
+from pyknic.lib.typing import GenericFunc, P, R
+
+
+class CriticalSectionError(Exception):
+    """ An exception that may be raised if a lock for a critical section could not be acquired
+    """
+    pass
+
+
+def acquire_lock(lock: threading.Lock, timeout: typing.Union[int, float, None] = None) -> bool:
+    """ This function overload the original :meth:`.Lock.acquire` method that has two arguments ('blocking'
+    and 'timeout') with the one argument only - 'timeout'. This argument may be set to None now
+
+    :param lock: a lock that should be gained
+
+    :param timeout: timeout that this function should wait in order to gain a lock. If the "timeout" is not
+    specified (is None) then wait forever in a blocking mode till a lock is gained, if the "timeout" is a
+    positive value this is a period of time in seconds that this function will wait for a lock in a blocking mode.
+    If a non-positive value is set then this function will return immediately even if lock was not gained
+    :type timeout: int | float | None
+
+    :return: whether a specified lock is gained or not
+    """
+    if timeout is None:
+        blocking = True
+        timeout = -1
+    elif timeout <= 0:
+        blocking = False
+        timeout = -1
+    else:
+        blocking = True
+    return lock.acquire(blocking=blocking, timeout=timeout)
+
+
+def critical_section_dynamic_lock(
+    lock_fn: typing.Callable[..., threading.Lock],
+    timeout: typing.Union[int, float, None] = None
+) -> typing.Callable[[GenericFunc[P, R]], GenericFunc[P, R]]:
+    """ Protect a function with a lock, that was get from the specified function. If a lock can not be acquired, then
+    no function call will be made
+
+    :param lock_fn: callable that returns a lock, with which a function may be protected
+    :param timeout: the same as 'timeout' in the :func:`.acquire_lock` function
+    :return: decorator with which a target function may be protected
+    """
+
+    def first_level_decorator(
+        decorated_function: typing.Callable[..., typing.Any]
+    ) -> typing.Callable[..., typing.Any]:
+        def second_level_decorator(
+            original_function: typing.Callable[..., typing.Any], *args: typing.Any, **kwargs: typing.Any
+        ) -> typing.Any:
+            lock = lock_fn(*args, **kwargs)
+            if acquire_lock(lock, timeout=timeout) is True:
+                try:
+                    result = original_function(*args, **kwargs)
+                    return result
+                finally:
+                    lock.release()
+
+            raise CriticalSectionError('Unable to lock a critical section')
+
+        return decorator.decorator(second_level_decorator)(decorated_function)
+    return first_level_decorator
+
+
+def critical_section_lock(
+    lock: threading.Lock, timeout: typing.Union[int, float, None] = None
+) -> typing.Callable[[GenericFunc[P, R]], GenericFunc[P, R]]:
+    """ A wrapper for :func:`.critical_section_dynamic_lock` function call, but with a static lock object
+    instead of a function that returns a lock
+
+    :param lock: lock with which a function will be protected
+    :param timeout: the same as 'timeout' in the :func:`.critical_section_dynamic_lock` function
+
+    :return: decorator with which a target function may be protected
+    """
+
+    def lock_getter(*args: typing.Any, **kwargs: typing.Any) -> threading.Lock:
+        return lock
+
+    return critical_section_dynamic_lock(lock_fn=lock_getter, timeout=timeout)
+
+
+class CriticalResource:
+    """ Class for simplifying thread safety for a shared object. Each class instance holds its own lock object
+    that :meth:`.CriticalResource.critical_section` method will be used to protect bounded methods.
+    Bounded methods are allowed to be protected only. Static methods or class methods are unsupported
+    """
+
+    class LockFreeContext:
+        """ This class may be used as a context that gets a monopoly access to a critical resource
+        (an :class:`.CriticalResource` object) and all the methods of that resource won't try to access already
+        gained lock and won't cause a deadlock.
+
+        :note: this object doesn't protect itself from a race condition. So this object usage must be limited to a
+        single thread only
+        """
+
+        def __init__(self, critical_resource: 'CriticalResource', timeout: typing.Union[int, float, None] = None):
+            """ Create a new context object
+
+            :param critical_resource: resource that this context should access and protect
+            :param timeout: the same as 'timeout' in the :func:`.acquire_lock` function
+            """
+
+            self.__resource = critical_resource
+            self.__timeout = timeout
+            self.__unlock = False
+
+        def __getattr__(self, item: str) -> typing.Any:
+            """ "Proxy" access to an original resource. If the result is protected by a lock with
+            :meth:`.CriticalResource.critical_section` decorator then an original (lock free) method is returned
+
+            :raise CriticalSectionError: if a resource was not locked before this access
+            """
+            if not self.__unlock:
+                raise CriticalSectionError(
+                    'A resource access error. A lock to the resource must be gotten at first!'
+                )
+
+            r_item = object.__getattribute__(self.__resource, item)
+
+            try:
+                return functools.partial(r_item.__pyknic_lock_free_fn__, r_item.__self__)
+            except AttributeError:
+                pass
+            return r_item
+
+        def __enter__(self) -> typing.Self:
+            """ Enter this context and lock a resource
+
+            :raise CriticalSectionError: if a resource was not locked during a specified timeout
+            """
+            if not acquire_lock(self.__resource.thread_lock(), self.__timeout):
+                raise CriticalSectionError('Unable to lock a critical section')
+            self.__unlock = True
+            return self
+
+        def __exit__(
+            self,
+            exc_type: typing.Optional[typing.Type[BaseException]],
+            exc_val: typing.Optional[BaseException],
+            exc_tb: typing.Optional[types.TracebackType]
+        ) -> None:
+            """ Exit a context and release a lock
+            """
+            if self.__unlock is True:
+                self.__unlock = False
+                self.__resource.thread_lock().release()
+
+        def __del__(self) -> None:
+            """ If a resource is locked still then unlock it
+            """
+            if self.__unlock is True:
+                self.__unlock = False
+                self.__resource.thread_lock().release()
+
+    def __init__(self) -> None:
+        """ Create a lock
+        """
+        self.__lock = threading.Lock()
+
+    def thread_lock(self) -> threading.Lock:
+        """ Return a lock with which a bounded methods may be protected
+        """
+        return self.__lock
+
+    def critical_context(self, timeout: typing.Union[int, float, None] = None) -> 'LockFreeContext':
+        """ Create a lock free context for this object
+
+        :param timeout: the same as 'timeout' in the :meth:`.CriticalResource.LockFreeContext.__init__` method
+        """
+        return CriticalResource.LockFreeContext(self, timeout=timeout)
+
+    @staticmethod
+    def critical_section(
+        timeout: typing.Union[int, float, None] = None
+    ) -> typing.Callable[[GenericFunc[P, R]], GenericFunc[P, R]]:
+        """ Decorate a method with a lock protection (class methods and static methods are unsupported)
+
+        :param timeout: the same as 'timeout' in the :func:`.critical_section_dynamic_lock` function
+        """
+
+        def lock_fn(self: CriticalResource, *args: typing.Any, **kwargs: typing.Any) -> threading.Lock:
+            if isinstance(self, CriticalResource) is False:
+                raise TypeError(
+                    'Invalid object type. It must be inherited from CriticalResource class and'
+                    ' decorated method must be bounded'
+                )
+            return self.thread_lock()
+
+        def decorator_fn(
+            decorated_function: typing.Callable[..., typing.Any]
+        ) -> typing.Callable[..., typing.Any]:
+            decorated_function.__pyknic_lock_free_fn__ = decorated_function  # type: ignore[attr-defined] # we are
+            # force the attribute to be
+            return critical_section_dynamic_lock(lock_fn=lock_fn, timeout=timeout)(decorated_function)
+
+        return decorator_fn
---
+++ tests/pytest/lib/thread_test.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+
+import gc
+import functools
+import pytest
+import threading
+import typing
+
+from pyknic.lib.thread import acquire_lock, critical_section_dynamic_lock, critical_section_lock, CriticalResource
+from pyknic.lib.thread import CriticalSectionError
+
+
+def test_acquire_lock() -> None:
+    lock = threading.Lock()
+    assert(acquire_lock(lock) is True)  # blocking mode - may wait forever
+    assert(acquire_lock(lock, timeout=-1) is False)  # lock is already locked and non-blocking mode is used
+    assert(acquire_lock(lock, timeout=1) is False)  # lock is already locked and blocking mode is used
+    lock.release()
+    assert(acquire_lock(lock, timeout=-1) is True)
+
+
+@pytest.mark.parametrize(
+    "threads_num, increments, lock_decorator", [
+        (50, 50, functools.partial(critical_section_dynamic_lock, lock_fn=lambda: threading.Lock()), ),
+        (50, 50, functools.partial(critical_section_lock, threading.Lock()), )
+    ]
+)
+def test_critical_section(
+    threads_num: int, increments: int, lock_decorator: typing.Callable[..., typing.Callable[..., typing.Any]]
+) -> None:
+
+    counter = 0
+
+    def shared_counter_fn() -> None:
+        nonlocal counter
+        for i in range(increments):
+            counter += 1
+
+    thread_safe_fn = lock_decorator()(shared_counter_fn)
+
+    threads = [threading.Thread(target=thread_safe_fn) for _ in range(threads_num)]
+    for th in threads:
+        th.start()
+
+    for th in threads:
+        th.join()
+
+    assert(counter == (threads_num * increments))
+
+
+def test_critical_section_timeout() -> None:
+    lock = threading.Lock()
+    lock.acquire()
+
+    thread_dynamic_safe_fn = critical_section_dynamic_lock(lambda: lock, timeout=3)(lambda: None)
+    with pytest.raises(CriticalSectionError):
+        thread_dynamic_safe_fn()
+
+    thread_safe_fn = critical_section_lock(lock, timeout=3)(lambda: None)
+    with pytest.raises(CriticalSectionError):
+        thread_safe_fn()
+
+    lock.release()
+    thread_dynamic_safe_fn()
+    thread_safe_fn()
+
+
+class SharedResourceSample(CriticalResource):
+
+    def __init__(self) -> None:
+        CriticalResource.__init__(self)
+        self.counter = 0
+
+    @CriticalResource.critical_section(timeout=3)
+    def increase(self) -> None:
+        self.counter += 1
+
+
+class TestCriticalResource:
+
+    @pytest.mark.parametrize("threads_num, repeats", [(50, 50)])
+    def test(self, threads_num: int, repeats: int) -> None:
+        sr = SharedResourceSample()
+        assert(sr.counter == 0)
+
+        def thread_fn_increase() -> None:
+            for _ in range(repeats):
+                sr.increase()
+
+        threads = [threading.Thread(target=thread_fn_increase) for _ in range(threads_num)]
+        for th in threads:
+            th.start()
+
+        for th in threads:
+            th.join()
+
+        assert(sr.counter == (threads_num * repeats))
+
+    def test_exceptions(self) -> None:
+        class A:
+            @CriticalResource.critical_section()
+            def foo(self) -> None:
+                pass
+        with pytest.raises(TypeError):
+            A().foo()
+
+
+class TestLockFreeContext:
+
+    @pytest.mark.parametrize("threads_num, repeats", [(50, 50)])
+    def test(self, threads_num: int, repeats: int) -> None:
+        sr = SharedResourceSample()
+        assert(sr.counter == 0)
+
+        def thread_fn_increase() -> None:
+            for i in range(repeats):
+                with sr.critical_context(timeout=1) as c1:
+                    c1.increase()
+
+        threads = [threading.Thread(target=thread_fn_increase) for x in range(threads_num)]
+        for th in threads:
+            th.start()
+
+        for th in threads:
+            th.join()
+
+        assert(sr.counter == (threads_num * repeats))
+
+        class A(CriticalResource):
+            @classmethod
+            def foo(cls, i: int) -> int:
+                return 2 * i
+
+        a = A()
+        with a.critical_context() as c2:
+            assert(c2.foo(1) == 2)  # access to original attributes works still
+
+    def test_exceptions(self) -> None:
+        sr = SharedResourceSample()
+        c1 = sr.critical_context(timeout=1)
+
+        with c1:
+            pass  # this is ok
+        with pytest.raises(CriticalSectionError):
+            with c1:
+                with c1:  # there is no way to lock a context twice
+                    pass
+
+        c2 = sr.critical_context(timeout=1)
+        with c1:
+            pass  # this is fine still
+        with pytest.raises(CriticalSectionError):
+            with c1:
+                with c2:  # there is no way to get in a critical section even from different contexts
+                    pass
+
+        with c1:
+            pass  # this doesn't do anything bad
+
+        with pytest.raises(CriticalSectionError):
+            with c1:
+                sr.increase()  # this won't work either
+
+        c1.__enter__()  # the object is "locked"
+        with pytest.raises(CriticalSectionError):
+            sr.increase()  # the object is locked definitely
+        del c1
+        gc.collect()
+        sr.increase()  # the object was unlocked automatically
+
+        with pytest.raises(CriticalSectionError):
+            _ = c2.increase  # access to a underlined methods are not allowed without a lock
