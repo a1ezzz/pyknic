6132f188891cc99e621b8ac5c4d00a4745dd14c8
Ildar Gafurov <dev@binblob.com>
2024-10-29 21:24:07 +0300

    lib/signals/proxy.py -- a new class -- QueueProxy

--- pyknic/lib/signals/proxy.py
+++ pyknic/lib/signals/proxy.py
@@ -19,11 +19,17 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pyknic.  If not, see <http://www.gnu.org/licenses/>.
 
+import functools
+import threading
 import typing
+import queue
 import weakref
 
-from pyknic.lib.signals.proto import SignalCallbackType, SignalProxyProto
+
+from pyknic.lib.signals.proto import SignalCallbackType, SignalProxyProto, SignalSourceProto, Signal
 from pyknic.lib.signals.extra import CallbackWrapper
+
+from pyknic.lib.tasks.proto import TaskProto
 
 
 class SignalProxy(SignalProxyProto):
@@ -35,6 +41,7 @@ class SignalProxy(SignalProxyProto):
 
         :param wrapper_factory: a class is used as a callback wrapper
         """
+        SignalProxyProto.__init__(self)
         self.__callbacks: weakref.WeakValueDictionary[SignalCallbackType, SignalCallbackType] = \
             weakref.WeakValueDictionary()
         self.__wrapper_factory = wrapper_factory if wrapper_factory else CallbackWrapper
@@ -56,3 +63,108 @@ class SignalProxy(SignalProxyProto):
 
         for i in to_remove:
             self.__callbacks.pop(i)
+
+
+class QueueProxy(SignalProxy, TaskProto):
+    """ This "proxy" helps to execute "proxified" callbacks in a dedicated thread
+    """
+
+    class QueueWrapper(CallbackWrapper):
+        """ Alternate :class:`.CallbackWrapper` implementation that work in conjunction with the :class:`.QueueProxy`
+        """
+
+        def __init__(self, callback: SignalCallbackType, weak_callback: bool = False):
+            """ Create a new wrapper
+
+            :param callback: same as 'callback' in :meth:`.CallbackWrapper.__init__` method
+            :param weak_callback: same as 'weak_callback' in :meth:`.CallbackWrapper.__init__` method
+            """
+            CallbackWrapper.__init__(self, callback, weak_callback=weak_callback)
+
+            self.__queue: typing.Optional[queue.Queue[typing.Callable[[], None]]] = None
+            self.__running: typing.Optional[threading.Event] = None
+
+        def setup_wrapper(
+            self,
+            queue_value: queue.Queue[typing.Callable[[], None]],
+            running_event: threading.Event
+        ) -> None:
+            """ Setup this wrapper. Without this setup a wrapper will crash
+            """
+            assert(not self.__queue)
+            assert(not self.__running)
+
+            self.__queue = queue_value
+            self.__running = running_event
+
+        def _pre_hook(
+            self, callback: SignalCallbackType, source: SignalSourceProto, signal: Signal, value: typing.Any
+        ) -> bool:
+            """ This pre-hook always returns False (a callback will be skipped from follow-on execution) and submits
+            the callback to a queue inside a :class:`.QueueProxy` object
+
+            :param callback: a callback that is proxying
+            :param source: signal origin
+            :param signal: emitted signal
+            :param value: a value of an emitted signal
+            """
+            assert(self.__queue)
+            assert(self.__running)
+
+            if self.__running.is_set():
+                self.__queue.put(functools.partial(callback, source, signal, value))
+            return False
+
+    def __init__(self, flash_flush: bool = False) -> None:
+        """ Create a new proxy
+
+        :param flash_flush: if True then every submitted callback will be executed just before the stop event.
+        Such callbacks may be omitted otherwise
+        """
+        SignalProxy.__init__(self, wrapper_factory=QueueProxy.QueueWrapper)
+        TaskProto.__init__(self)
+
+        self.__queue: queue.Queue[typing.Callable[[], None]] = queue.Queue()
+
+        self.__running = threading.Event()
+        self.__flash_flush = flash_flush
+
+    def proxy(self, callback: SignalCallbackType) -> SignalCallbackType:
+        """ The :meth:`.SignalProxyProto.proxy` method implementation
+        """
+        callback_wrapper: QueueProxy.QueueWrapper = \
+            SignalProxy.proxy(  # type: ignore[assignment]  # the SignalProxy.proxy method will return QueueWrapper
+                self, callback
+            )
+
+        callback_wrapper.setup_wrapper(self.__queue, self.__running)
+        return callback_wrapper
+
+    def __flush(self) -> None:
+        """ Execute all the functions that have been stored in the queue
+        """
+        callback = self.__queue.get(False)
+        while callback:  # type: ignore[truthy-function]  # mypy is incorrect here since the get method is called
+            # with the False value
+            if not self.__flash_flush:
+                callback()
+            self.__queue.task_done()
+            callback = self.__queue.get(False)
+
+    def start(self) -> None:
+        """ The :meth:`.TaskProto.start` method implementation
+        """
+        self.__running.set()
+        while self.__running.is_set():
+            next_callback = self.__queue.get()
+            next_callback()
+            self.__queue.task_done()
+
+        self.__flush()
+
+    def stop(self) -> None:
+        """ The :meth:`.TaskProto.stop` method implementation
+        """
+        self.__running.clear()
+        self.__queue.put(lambda: None)  # force the "self.__queue.get()" to return something
+        self.__queue.join()
--- tests/pytest/lib/signals/proxy_test.py
+++ tests/pytest/lib/signals/proxy_test.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+
 import gc
+import threading
 import typing
 
 from pyknic.lib.signals.extra import CallbackWrapper
-from pyknic.lib.signals.proto import Signal, SignalCallbackType, SignalSourceProto
+from pyknic.lib.signals.proto import Signal, SignalCallbackType, SignalSourceProto, SignalProxyProto
 from pyknic.lib.signals.source import SignalSource
-from pyknic.lib.signals.proxy import SignalProxy
+from pyknic.lib.tasks.threaded_task import ThreadedTask
+
+from pyknic.lib.signals.proxy import SignalProxy, QueueProxy
 
 
 class TestSignalProxy:
@@ -70,8 +74,9 @@ class TestSignalProxy:
 
             def _pre_hook(
                 self, callback: SignalCallbackType, source: SignalSourceProto, signal: Signal, value: typing.Any
-            ) -> None:
+            ) -> bool:
                 callbacks_registry.callback('test_callback')()
+                return True
 
         signal_proxy = SignalProxy(wrapper_factory=CustomCallbackWrapper)
         source = Source()
@@ -88,3 +93,54 @@ class TestSignalProxy:
         source.emit(Source.signal1)
         assert(signals_registry.dump(True) == [])
         assert(callbacks_registry.calls('test_callback') == 1)
+
+
+class TestQueueProxy:
+
+    def test_plain(self) -> None:
+        queue_proxy = QueueProxy()
+        threaded_task = ThreadedTask(queue_proxy)
+
+        threaded_task.start()
+        threaded_task.stop()
+        threaded_task.join()
+
+        assert(isinstance(queue_proxy, SignalProxyProto))
+
+    def test_signals(self) -> None:
+        queue_proxy = QueueProxy()
+        threaded_task = ThreadedTask(queue_proxy)
+        threaded_task.start()
+
+        class Source(SignalSource):
+            signal1 = Signal()
+            signal2 = Signal(int)
+
+        lock = threading.Lock()
+        results = []
+
+        def callback(source: SignalSourceProto, signal: Signal, value: typing.Any) -> None:
+            nonlocal results, lock
+            with lock:
+                results.append((source, signal, value))
+
+        source1 = Source()
+        source2 = Source()
+
+        source1.callback(Source.signal1, queue_proxy.proxy(callback))
+        source1.callback(Source.signal2, queue_proxy.proxy(callback))
+        source2.callback(Source.signal1, queue_proxy.proxy(callback))
+
+        source1.emit(Source.signal1)
+        source1.emit(Source.signal2, 0)
+        source2.emit(Source.signal1)
+        source2.emit(Source.signal2, 1)
+
+        threaded_task.stop()
+        threaded_task.join()
+
+        assert(results == [
+            (source1, Source.signal1, None),
+            (source1, Source.signal2, 0),
+            (source2, Source.signal1, None),
+        ])
