d15c46f3424bc10ff2eb808ac0b73dc0c2364249
Ildar Gafurov <dev@binblob.com>
2025-06-12 14:41:59 +0300

    tests/pytest/asyncio_helpers.py: TODO removed

--- tests/pytest/asyncio_helpers.py
+++ tests/pytest/asyncio_helpers.py
@@ -19,8 +19,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pyknic.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO: document the code
-
 import asyncio
 import pytest
 import typing
