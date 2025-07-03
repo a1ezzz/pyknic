a3281a87eab319e4861af4ce0851a1fdaf781280
Ildar Gafurov <dev@binblob.com>
2025-07-03 22:13:14 +0300

    pyknic_loop.sh: bug-o-fix

--- pyknic_loop.sh
+++ pyknic_loop.sh
@@ -2,6 +2,8 @@
 
 set -u
 
+export PYTHONPATH="$(dirname ${0})"
+
 trap "kill -- -${BASHPID}" EXIT  # forces watchmedo to stop when this script ends
 
 watchmedo shell-command --patterns='**/*.py' --recursive --command='killall -s KILL pyknic' &
