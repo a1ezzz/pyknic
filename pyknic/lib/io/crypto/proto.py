021dd91a737e0f2c6f832589ba6e8537937f16c8
Ildar Gafurov <dev@binblob.com>
2026-01-05 18:03:10 +0300

    pyknic/lib/io/crypto/proto.py: BlockPaddingProto -- new proto class for data padding

--- pyknic/lib/io/crypto/proto.py
+++ pyknic/lib/io/crypto/proto.py
@@ -19,9 +19,12 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pyknic.  If not, see <http://www.gnu.org/licenses/>.
 
+import typing
+
 from abc import ABCMeta, abstractmethod
 
 from pyknic.lib.io import IOGenerator
+from pyknic.lib.verify import verify_value
 
 
 class CipherProto(metaclass=ABCMeta):
@@ -29,27 +32,54 @@ class CipherProto(metaclass=ABCMeta):
     """
 
     @abstractmethod
-    def block_size(self):
+    def block_size(self) -> typing.Optional[int]:
         """Return a size of a block that may be encrypted or decrypted.
 
         :return: int (in bytes) or None if cipher is able to encrypt/decrypt block with any length
-		"""
+        """
         raise NotImplementedError('This method is abstract')
 
     @abstractmethod
     def encrypt(self, data: IOGenerator) -> IOGenerator:
         """ Encrypt the given data
 
-		:param data: data to encrypt. The size of the data must be multiple of :meth:`.CipherProto.block_size`
-		"""
+        :param data: data to encrypt. The size of the data must be multiple of :meth:`.CipherProto.block_size`
+        """
         raise NotImplementedError('This method is abstract')
 
     @abstractmethod
     def decrypt(self, data: IOGenerator) -> IOGenerator:
         """ Decrypt the given data
 
-		:param data: data to decrypt. The size of the data must be multiple of :meth:`.CipherProto.block_size`
+        :param data: data to decrypt. The size of the data must be multiple of :meth:`.CipherProto.block_size`
 
-		:return: bytes
-		"""
+        :return: bytes
+        """
         raise NotImplementedError('This method is abstract')
+
+
+class BlockPaddingProto(metaclass=ABCMeta):
+    """ Padding/reverse padding class prototype
+    """
+
+    @abstractmethod
+    @verify_value(block_size=lambda x: x > 0)
+    def pad(self, data: IOGenerator, block_size: int) -> IOGenerator:
+        """ Pad given data to given size
+
+        :param data: data to pad
+        :param block_size: size to pad
+        :return: bytes
+        """
+        raise NotImplementedError("This method is abstract")
+
+    @abstractmethod
+    @verify_value(block_size=lambda x: x > 0)
+    def undo_pad(self, data: IOGenerator, block_size: int) -> IOGenerator:
+        """Remove pads and return original data
+
+        :param data: data to remove pads from
+        :param block_size: size data aligned to
+        :return: bytes
+        """
+        raise NotImplementedError("This method is abstract")
--- tests/pytest/lib/io/crypto/proto_test.py
+++ tests/pytest/lib/io/crypto/proto_test.py
@@ -1,7 +1,7 @@
 
 import pytest
 
-from pyknic.lib.io.crypto.proto import CipherProto
+from pyknic.lib.io.crypto.proto import CipherProto, BlockPaddingProto
 
 
 def test_abstract() -> None:
@@ -9,3 +9,7 @@ def test_abstract() -> None:
     pytest.raises(NotImplementedError, CipherProto.block_size, None)
     pytest.raises(NotImplementedError, CipherProto.encrypt, None, (x for x in [b'bbbb']))
     pytest.raises(NotImplementedError, CipherProto.decrypt, None, (x for x in [b'bbbb']))
+
+    pytest.raises(TypeError, BlockPaddingProto)
+    pytest.raises(NotImplementedError, BlockPaddingProto.pad, None, (x for x in [b'bbbb']), 1)
+    pytest.raises(NotImplementedError, BlockPaddingProto.undo_pad, None, (x for x in [b'bbbb']), 1)
